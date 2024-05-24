6520ebe33216601ff1722b9146f90af988f115cb
uprock-media <166035022+uprock-media@users.noreply.github.com>
2024-05-24 12:41:00 +0500

    Update open-modal.js

--- open-modal.js
+++ open-modal.js
@@ -1,28 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Находим все элементы, которые должны открывать модальное окно
     var storyItems = document.querySelectorAll('.stories-item');
+    var body = document.querySelector('body');
 
-    // Для каждого элемента устанавливаем обработчик клика
     storyItems.forEach(function(item) {
         item.addEventListener('click', function(event) {
-            event.preventDefault(); // Предотвращаем стандартное поведение ссылки, если это необходимо
+            event.preventDefault();
 
-            // Находим модальное окно и изменяем его стиль для отображения
             var modal = document.querySelector('.stories-modal-slider');
             if (modal) {
-                modal.style.display = 'flex'; // Или любой другой стиль, который делает его видимым
+                modal.style.display = 'flex';
+                body.classList.add('no-scroll');
             }
         });
     });
 
-    // Настройка закрытия модального окна
-    var closeModalElement = document.querySelector('.stories-modal_bg'); // Изменено на stories-modal_bg
-    if (closeModalElement) {
-        closeModalElement.addEventListener('click', function() {
+    var closeModalElements = document.querySelectorAll('.stories-modal_bg, .close-modal-story');
+    closeModalElements.forEach(function(element) {
+        element.addEventListener('click', function() {
             var modal = document.querySelector('.stories-modal-slider');
             if (modal) {
-                modal.style.display = 'none'; // Скрываем модальное окно
+                modal.style.display = 'none';
+                body.classList.remove('no-scroll');
             }
         });
-    }
+    });
 });
